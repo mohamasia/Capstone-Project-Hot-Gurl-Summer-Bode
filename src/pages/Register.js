cefabbdc2f624f5a684257810d60cf57c513e3d1
Asia <mohamedasia1@gmail.com>
2024-02-27 00:48:10 -0500

    Add registration functionality to Register component

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -8,6 +8,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from "../api/axios";
 import "./register.scss";
 
+// Add registration functionality to Register component
 const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
 const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
 const REGISTER_URL = "/register";
