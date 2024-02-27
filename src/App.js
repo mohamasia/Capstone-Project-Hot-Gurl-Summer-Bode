9030a463a02de92f3b437c2672d8b00b16b13014
Asia <mohamedasia1@gmail.com>
2024-02-27 01:12:24 -0500

    Renders the application and routes

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Register from "./pages/Register";
 
+// Renders the application and routes
 const App = () => {
   let loggedIn = true;
 
